bb44114a77d223e4fa0a011208a1c28e49b727f7
loboo34 <rayovers40@gmail.com>
2025-12-07 17:49:38 +0300

    websoket handler

--- internal/websoket/client.go
+++ internal/websoket/client.go
@@ -2,9 +2,8 @@ package websoket
 
 import "github.com/gorilla/websocket"
 
-
-type Client struct{
-	 hub  *Hub
+type Client struct {
+	hub  *Hub
 	User string `bson:"user" json:"user"`
 	Conn *websocket.Conn
 	Send chan Message
--- internal/websoket/handler.go
+++ internal/websoket/handler.go
@@ -1,1 +1,33 @@
 package websoket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func websk(hub *Hub, w http.ResponseWriter, r *http.Request, userID string) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	client := &Client{
+		hub:  hub,
+		User: userID,
+		Conn: conn,
+		Send: make(chan Message, 250),
+	}
+
+	hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
+
+}
