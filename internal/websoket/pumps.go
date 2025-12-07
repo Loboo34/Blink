4cd6e280be1bc818f36bbc6acb547b0a95b1990e
loboo34 <rayovers40@gmail.com>
2025-12-07 17:28:32 +0300

    Pumps

--- internal/websoket/pumps.go
+++ internal/websoket/pumps.go
@@ -1,1 +1,34 @@
 package websoket
+
+
+
+
+func (c *Client) readPump(){
+	defer func ()  {
+		c.hub.unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		var msg Message
+		err := c.Conn.ReadJSON(&msg)
+		if err != nil{
+			break
+		}
+
+		msg.SenderID = c.User
+		c.hub.broadcast <- msg
+	}
+
+}
+
+
+func (c *client) writePump(){
+	defer c.Conn.Close()
+
+	for msg := range c.send {
+		 if err := c.conn.WriteJSON(msg); err != nil {
+            break
+        }
+	}
+}
